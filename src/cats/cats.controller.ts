6237ff7e1861b83665d50b1311525bb288e3463e
taketaku <vbvbsp48@gmail.com>
2021-07-06 09:24:23 +0900

    git add exception

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,12 @@
-import { Body } from '@nestjs/common';
+import {
+  Body,
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+} from '@nestjs/common';
 import { Controller, Post, Get } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/filter/http-exception.filtter';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cats.dto';
 
@@ -8,12 +15,15 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UseFilters(new HttpExceptionFilter())
   async create(@Body() createCatDto: CreateCatDto) {
+    throw new ForbiddenException();
     this.catsService.create(createCatDto);
   }
 
   @Get()
   findAll() {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     // return this.catsService.findAll();
     return 'All cats';
   }
